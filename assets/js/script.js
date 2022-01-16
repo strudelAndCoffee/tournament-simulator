f31cd6e3b3bc10f1cbf37d6c2027f7ad90b1e0cb
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-01-16 04:02:19 -0600

    added tie breaker round calculation, add match winners/ties  results

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,9 @@ var shuffleArray = function (array) {
   }
 };
 
+var roundResults = [];
+var matchResults = [];
+
 // Player object and Groups
 class Player {
   constructor(name, type) {
@@ -31,15 +34,15 @@ class Player {
     this.lose = function () {
       this.gamesLost--;
     };
-    this.tie = function() {
+    this.tie = function () {
       this.gamesTied++;
-    }
+    };
     this.rank = 0;
     this.Red = 0;
     this.Blue = 0;
     this.Green = 0;
     this.Yellow = 0;
-    this.opp = [];
+    this.oppAdv = [];
     this.adv = function (opp) {
       switch (opp) {
         case "Red":
@@ -55,12 +58,12 @@ class Player {
           this.Yellow++;
           break;
         default:
-          this.opp.push(opp);
+          this.oppAdv.push(opp.name);
           break;
       }
     };
   }
-};
+}
 
 var player1 = new Player("Angus", "Red");
 var player2 = new Player("Bobby", "Red");
@@ -133,77 +136,131 @@ var group4 = {
 };
 
 var runGroupStage = function (group) {
-  var match1 = [group.p1, group.p2, group.p3, group.p4, "Match 1: "];
-  var match2 = [group.p1, group.p3, group.p2, group.p4, "Match 2: "];
-  var match3 = [group.p4, group.p1, group.p3, group.p2, "Match 3: "];
-  var match4 = [group.p2, group.p1, group.p4, group.p3, "Match 4: "];
-  var match5 = [group.p3, group.p1, group.p4, group.p2, "Match 5: "];
-  var match6 = [group.p1, group.p4, group.p2, group.p3, "Match 6: "];
+  var match1 = [group.p1, group.p2, group.p3, group.p4, 1];
+  var match2 = [group.p1, group.p3, group.p2, group.p4, 2];
+  var match3 = [group.p4, group.p1, group.p3, group.p2, 3];
+  var match4 = [group.p2, group.p1, group.p4, group.p3, 4];
+  var match5 = [group.p3, group.p1, group.p4, group.p2, 5];
+  var match6 = [group.p1, group.p4, group.p2, group.p3, 6];
   var groupMatches = [match1, match2, match3, match4, match5, match6];
 
   for (var i = 0; i < groupMatches.length; i++) {
     var currentMatch = groupMatches[i];
-    runGroupMatch(currentMatch);
+    runGroupMatches(currentMatch);
   }
 };
 
-var runGroupMatch = function(match) {
-  var matchNumber = match[4];
+var runGroupMatches = function (match) {
+  var matchNumBroadcast = "--Match " + match[4] + ": ";
+  var matchId = "m" + match[4];
   var player1 = match[0];
   var player2 = match[1];
   var player3 = match[2];
   var player4 = match[3];
-  console.log(matchNumber);
-  console.log(player1, player2, player3, player4);
+  console.log(matchNumBroadcast);
+  console.log(player1.name, player2.name, player3.name, player4.name);
 
-  var runMatch = function(p1, p2) {
-    let round1 = [p1, p2, 1];
-    let round2 = [p2, p1, 2];
+  runMatch(player1, player2, matchId);
+  runMatch(player3, player4, matchId);
+};
 
-    runRound(round1);
-    runRound(round2);
-  };
+var runMatch = function (p1, p2, m) {
+  let round1 = [p1, p2, 1, m];
+  let round2 = [p2, p1, 2, m];
 
-  var runRound = function(round) {
-    let p1 = round[0];
-    let p2 = round[1];
-    let p1score = round[0].score();
-    let p2score = round[1].score();
-    let roundNum = "Round " + round[2] + "--- ";
-    let roundOutcome = "";
+  runRound(round1);
+  runRound(round2);
+};
 
-    if (p1score > p2score) {
-      p1.win();
-      p2.lose();
-      roundOutcome = roundNum + p1.name + " wins: " + p1score + " to " + p2score;
-      if (p1score >= 5 && p2score <= 2) {
-        p1.adv(p2);
-      }
+var runRound = function (round) {
+  let p1 = round[0];
+  let p2 = round[1];
+  let p1score = round[0].score();
+  let p2score = round[1].score();
+  let roundNum = "Round " + round[2] + "--- ";
+  let roundId = "r" + round[2];
+  let matchId = round[3];
+  let roundOutcome = [];
+  let roundOutcomeBroadcast = "";
+
+  if (p1score > p2score) {
+    p1.win();
+    p2.lose();
+    roundOutcomeBroadcast = roundNum + p1.name + " wins: " + p1score + " to " + p2score;
+    roundOutcome.push(matchId, roundId, p1, 1, p2);
+    if (p1score >= 5 && p2score <= 2) {
+      p1.adv(p2);
     }
-    else if (p2score > p1score) {
-      p2.win();
-      p1.lose();
-      roundOutcome = roundNum + p2.name + " wins: " + p2score + " to " + p1score;
-      if (p2score >= 5 && p1score <= 2) {
-        p2.adv(p1);
-      }
+  } else if (p2score > p1score) {
+    p2.win();
+    p1.lose();
+    roundOutcomeBroadcast = roundNum + p2.name + " wins: " + p2score + " to " + p1score;
+    roundOutcome.push(matchId, roundId, p2, 1, p1);
+    if (p2score >= 5 && p1score <= 2) {
+      p2.adv(p1);
     }
-    else if (p1score === p2score) {
-      p1.tie();
-      p2.tie();
-      roundOutcome = roundNum + p1.name + " ties " + p2.name + ": " + p1score + " to " + p2score;
-    }
-
-    console.log(roundOutcome);
-  };
-
-  var runTieBreaker = function(round) {
-    console.log(round);
-    console.log("round 3: the tie breaker");
+  } else if (p1score === p2score) {
+    p1.tie();
+    p2.tie();
+    roundOutcomeBroadcast = roundNum + p1.name + " ties " + p2.name + ": " + p1score + " to " + p2score;
+    roundOutcome.push(matchId, roundId, p1, 0, p2);
   }
 
-  runMatch(player1, player2);
-  runMatch(player3, player4);
+  roundResults.push(roundOutcome);
+  console.log(roundOutcomeBroadcast);
+};
+
+var calculateResults = function() {
+
+  let matchWinners = [];
+  let matchTies = [];
+
+  for (var i = 0; i < roundResults.length; i++) {
+
+    if (i % 2 !== 0) {
+      let r1 = roundResults[i -1];
+      let r2 = roundResults[i];
+      let matchNum = r1[0];
+      let p1 = r1[2];
+      let p2 = r1[4];
+      let r1w = r1[3];
+      let r2w = r2[3];
+      
+      if (r1w === 0 && r2w === 0) {
+        matchTies.push(matchNum, p1.name, p2.name);
+      }
+      else if (r1w === 1 && r2w === 0) {
+        runTieBreaker(matchNum, p1, p2, p1);
+      }
+      else if (r1w === 0 && r2w === 1) {
+        runTieBreaker(matchNum, p1, p2, p2);
+      }
+      else if (p1 === r2[4]) {
+        runTieBreaker(matchNum, p1, p2, 0);
+      }
+      else if (p1 === r2[2]) {
+        matchWinners.push(matchNum, p1.name);
+      }
+      else if (p2 === r2[2]) {
+        matchWinners.push(matchNum, p2.name);
+      }
+    }
+
+  }
+
+  matchWinners.unshift("won the match:");
+  matchTies.unshift("tied the match:");
+  matchResults.push(matchWinners, matchTies);
+  matchWinners = [];
+  matchTies = [];
+}
+
+var runTieBreaker = function (m, p1, p2, adv) {
+  let round = [m, p1, p2, adv];
+  console.log(round);
+  console.log("round 3: the tie breaker");
 };
 
 runGroupStage(group1);
+calculateResults();
+console.log(matchResults);
